d8d7bfbc3a3f5a0063fce272408820fe3cdd175b
y-ibuki <y.ibuki91@gmail.com>
2021-02-27 01:03:33 +0900

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,122 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from 'react'
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import axios from "axios";
+import styled from "styled-components";
+
+const CheckBox = styled.input.attrs( () => ({
+  type: 'checkbox',
+}))`
+  font-size: 30px;
+  margin-right: 5px;
+`
+
+const Label = styled.label`
+  font-size: 18px;
+  vertical-align: middle;
+  margin-left: 10px;
+`
+
+const PrefWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 0 30px;
+`
 
 export default function Home() {
+  const prefectureUrl = 'https://opendata.resas-portal.go.jp/api/v1/prefectures';
+  const populationUrl = 'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear'
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const [pref, setPref] = useState([])
+  const [popByPref, setPopByPref] = useState([])
+  const [selectedPref, setSelectedPref] = useState([])
+  useEffect( () => {
+    // 都道府県取得
+    const prefectures = axios.get( prefectureUrl, { headers: { 'X-API-KEY': apiKey } })
+    prefectures.then( res => {
+      const prefData = res.data.result
+      setPref(prefData)
+    })
+  }, [])
+
+  function addPopulation(prefCode, prefName) {
+    const populationByPrefecture =
+      axios.get( populationUrl + '?cityCode=-&prefCode=' + prefCode, { headers: { 'X-API-KEY': apiKey } })
+
+    populationByPrefecture.then( i => {
+      const data = i.data.result.data[0].data
+      const populationArray = []
+      data.map( i => {
+        populationArray.push(i.value)
+      })
+
+      const popByPrefData = {name: prefName, data: populationArray}
+      setPopByPref([...popByPref, popByPrefData])
+    })
+  }
+
+  function deletePopulation(prefCode, prefName) {
+    const popByPrefCopy = popByPref.slice()
+    const selectedPrefCopy = selectedPref.slice()
+    popByPref.map( (i, index) => {
+      if (i.name === prefName) {
+        popByPrefCopy.splice(index, 1)
+        setPopByPref(popByPrefCopy)
+      }
+    })
+
+    selectedPref.map( (i, index) => {
+      if (i === prefCode) {
+        selectedPrefCopy.splice(index, 1)
+        setSelectedPref(selectedPrefCopy)
+      }
+    })
+  }
+
+  function onClick(e) {
+    const prefValue = e.target.value.split(',')
+    const prefCode = prefValue[0]
+    const prefName = prefValue[1]
+    if (selectedPref.includes(prefCode)) {
+      // チェックを外した時
+      deletePopulation(prefCode, prefName)
+    } else {
+      // チェックした時
+      setSelectedPref([...selectedPref, prefCode])
+      addPopulation(prefCode, prefName)
+    }
+  }
+
+  const options = {
+    title: {
+      text: '総人口率'
+    },
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false
+        },
+        pointInterval: 5,
+        pointStart: 1965
+      }
+    },
+    series: popByPref
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div className="App">
+      <PrefWrapper>
+        {
+          pref.map( ({prefCode, prefName}) => {
+            return (
+              <Label key={prefCode}>
+                <CheckBox value={[prefCode, prefName]} onClick={onClick} />{prefName}
+              </Label>
+            )
+          })
+        }
+      </PrefWrapper>
+      <HighchartsReact highcharts={Highcharts} options={options}></HighchartsReact>
     </div>
   )
 }
